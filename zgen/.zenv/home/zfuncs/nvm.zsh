d70712f5335753ebde790877834ba55eb1b1e209
wilypomegranate <wilypomegranate@users.noreply.github.com>
2017-07-23 12:37:22 -0400

    Uncommented nvm function and fixed it to work correctly

--- zgen/.zenv/home/zfuncs/nvm.zsh
+++ zgen/.zenv/home/zfuncs/nvm.zsh
@@ -1,11 +1,11 @@
-# function nvm() {
-#     if [ -z ${NVM_DIR} ]
-#     then
-#         source ${NVM_DIR}/nvm.sh
-#         nvm "$@"
-#     elif [ -d ${HOME}/.nvm ]
-#     then
-#         source ${HOME}/.nvm/nvm.sh
-#         nvm "$@"
-#     fi
-# }
+function nvm() {
+    if [ -n "${NVM_DIR}" ]
+    then
+        source "${NVM_DIR}/nvm.sh"
+        nvm "$@"
+    elif [ -d "${HOME}/.nvm" ]
+    then
+        source "${HOME}/.nvm/nvm.sh"
+        nvm "$@"
+    fi
+}
