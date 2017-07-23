cbed424a9f8d51676528a9be275d10e2d90b839f
wilypomegranate <wilypomegranate@users.noreply.github.com>
2017-07-23 12:22:40 -0400

    Added nvm function to lazy load nvm

---
+++ zgen/.zenv/home/zfuncs/nvm.zsh
@@ -0,0 +1,11 @@
+# function nvm() {
+#     if [ -z ${NVM_DIR} ]
+#     then
+#         source ${NVM_DIR}/nvm.sh
+#         nvm "$@"
+#     elif [ -d ${HOME}/.nvm ]
+#     then
+#         source ${HOME}/.nvm/nvm.sh
+#         nvm "$@"
+#     fi
+# }
