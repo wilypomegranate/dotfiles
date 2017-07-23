7ded31f91340d2b6c7a356c8028d2ec86fc5aec2
wilypomegranate <wilypomegranate@users.noreply.github.com>
2017-07-23 12:18:51 -0400

    Added SAVEHIST to history file. I think this will work?:)

--- zgen/.zenv/home/zvars/history.zsh
+++ zgen/.zenv/home/zvars/history.zsh
@@ -1,5 +1,6 @@
 # Just let me have an infinite history!
 export HISTSIZE=9999999999999999
+export SAVEHIST=9999999999999999
 
 # Finally setting on .zhistory to be my history file
 # because work is setup that way and I don't want to merge them.
