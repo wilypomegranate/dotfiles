519095592a4a96d9d5f989518777550c03a55ad0
wilypomegranate <wilypomegranate@users.noreply.github.com>
2020-12-11 21:13:16 -0500

    eshell tweaks

--- emacs/.doom.d/eshell.el
+++ emacs/.doom.d/eshell.el
@@ -1,3 +1,6 @@
 ;;; ../dotfiles/emacs/.doom.d/eshell.el -*- lexical-binding: t; -*-
 
 (add-to-list 'eshell-visual-commands "top")
+(add-to-list 'eshell-visual-options '("git" "--help"))
+(add-to-list 'eshell-visual-subcommands '("git" "log" "diff" "show"))
+(setq eshell-destroy-buffer-when-process-dies t)
